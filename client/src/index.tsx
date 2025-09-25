f83e788dcb801b06f3fc050455434aa797b87e34
Maksim Krivosheev <mmkrivosheev@mail.ru>
2025-09-25 17:05:26 +0300

    i18n

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "@fontsource/roboto";
 import "./styles/index.scss";
+import "./i18n";
 
 const rootEl = document.getElementById("root")!;
 const root = createRoot(rootEl);
