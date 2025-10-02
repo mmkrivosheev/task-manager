01a667457c243565f639f44bc5e7c81e9e5dc975
Maksim Krivosheev <mmkrivosheev@mail.ru>
2025-10-02 04:17:43 +0300

    update configs

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -40,7 +40,6 @@ module.exports = {
 			app: path.resolve(__dirname, "src/app"),
 			assets: path.resolve(__dirname, "src/assets"),
 			entities: path.resolve(__dirname, "src/entities"),
-			features: path.resolve(__dirname, "src/features"),
 			layouts: path.resolve(__dirname, "src/layouts"),
 			pages: path.resolve(__dirname, "src/pages"),
 			shared: path.resolve(__dirname, "src/shared"),
