bb0eac88ca80f55cfcd64cc12bf059b3200dbda7
Maksim Krivosheev <mmkrivosheev@mail.ru>
2025-10-04 04:09:59 +0300

    update guest page

--- client/src/pages/GuestPage/GuestPage.tsx
+++ client/src/pages/GuestPage/GuestPage.tsx
@@ -3,11 +3,13 @@ import { Trans, useTranslation } from "react-i18next";
 import styles from "./GuestPage.module.scss";
 import { Button } from "shared/UI/Button";
 import { loginUser } from "entities/auth/authThunk";
-import { useAppDispatch } from "app/store/hooks";
-import { demoUser } from "pages/GuestPage/demoUser";
+import { useAppDispatch, useAppSelector } from "app/store/hooks";
 import { clearError } from "entities/auth/authSlice";
 
+export const demo = { email: "demo@example.com", password: "123456" };
+
 export function GuestPage() {
+	const { error } = useAppSelector(state => state.auth);
 	const dispatch = useAppDispatch();
 	const { t } = useTranslation();
 
@@ -18,18 +20,17 @@ export function GuestPage() {
 	}, [dispatch]);
 
 	const handleClick = async () => {
-		await dispatch(loginUser(demoUser));
+		await dispatch(loginUser(demo));
 	};
 
 	return (
-		<>
-			<div className={styles.guestWrapper}>
-				<p>{t("GuestPage.info")}</p>
-				<p>
-					<Trans i18nKey="GuestPage.demoInfo" components={[<i key="italic" />]} />
-				</p>
-				<Button onClick={handleClick}>{t("GuestPage.demoButton")}</Button>
-			</div>
-		</>
+		<div className={styles.guestWrapper}>
+			<p>{t("GuestPage.info")}</p>
+			<p>
+				<Trans i18nKey="GuestPage.demoInfo" components={[<i key="italic" />]} />
+			</p>
+			<Button onClick={handleClick}>{t("GuestPage.demoButton")}</Button>
+			{error && <p className={styles.errorMessage}>{t(error)}</p>}
+		</div>
 	);
 }
