217c42b727250749f40f48a38722b1dbae437c90
Maksim Krivosheev <mmkrivosheev@mail.ru>
2025-10-04 04:14:07 +0300

    update auth checking

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,20 +1,15 @@
 import { useEffect, useState } from "react";
 import { Router } from "./routes/Router";
-import { useAppDispatch } from "shared/hooks/redux";
-import { fetchCurrentUser } from "entities/User/authThunk";
-import { SplashScreen } from "shared/UI/SplashScreen";
+import { useAppDispatch } from "app/store/hooks";
+import { fetchCurrentUser } from "entities/auth/authThunk";
 
 export default function App() {
-	const [authLoading, setAuthLoading] = useState(true);
+	const [authCheck, setAuthCheck] = useState(true);
 	const dispatch = useAppDispatch();
 
 	useEffect(() => {
-		let timer: ReturnType<typeof setTimeout>;
-		dispatch(fetchCurrentUser()).then(() => {
-			timer = setTimeout(() => setAuthLoading(false), 1200);
-		});
-		return () => clearTimeout(timer);
-	}, []);
+		dispatch(fetchCurrentUser()).then(() => setAuthCheck(false));
+	}, [dispatch]);
 
-	return authLoading ? <SplashScreen /> : <Router />;
+	return authCheck || <Router />;
 }
