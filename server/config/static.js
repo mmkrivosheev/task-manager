06ffe0bc472d2e9aa75e110fa728a101bc21f4e6
Maksim Krivosheev <mmkrivosheev@mail.ru>
2025-10-10 11:07:47 +0300

    update options for browser caching

--- server/config/static.js
+++ server/config/static.js
@@ -1,13 +1,12 @@
 export const staticOptions = {
 	maxage: 86400000,
 	etag: true,
-	lastModified: true,
 	setHeaders: setCacheHeaders,
 };
 
 export function setCacheHeaders(res, filePath) {
 	if (filePath.endsWith(".html")) {
-		res.setHeader("Cache-Control", "no-store");
+		res.setHeader("Cache-Control", "private, no-cache");
 	} else {
 		res.setHeader("Cache-Control", "private, max-age=86400");
 	}
