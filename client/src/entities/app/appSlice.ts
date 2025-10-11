4e2aef74dbc6d6b2a8d277346a18184902b59b86
Maksim Krivosheev <mmkrivosheev@mail.ru>
2025-10-11 16:09:54 +0300

    update app slice

--- client/src/entities/app/appSlice.ts
+++ client/src/entities/app/appSlice.ts
@@ -3,6 +3,12 @@ import { IAppState, IToast } from "entities/app/types";
 
 const initialState: IAppState = {
 	toast: null,
+	modal: {
+		isOpen: false,
+		title: "",
+		type: null,
+		props: null,
+	},
 };
 
 const appSlice = createSlice({
@@ -15,8 +21,20 @@ const appSlice = createSlice({
 		clearToast(state) {
 			state.toast = null;
 		},
+		openModal: (state, action) => {
+			state.modal.isOpen = true;
+			state.modal.title = action.payload.title;
+			state.modal.type = action.payload.type;
+			state.modal.props = action.payload.props;
+		},
+		closeModal: state => {
+			state.modal.isOpen = false;
+			state.modal.title = null;
+			state.modal.type = null;
+			state.modal.props = null;
+		},
 	},
 });
 
-export const { showToast, clearToast } = appSlice.actions;
+export const { showToast, clearToast, openModal, closeModal } = appSlice.actions;
 export default appSlice.reducer;
--- client/src/entities/app/types.ts
+++ client/src/entities/app/types.ts
@@ -1,9 +1,19 @@
+import { ITaskCardProps } from "shared/UI/TaskCard/types";
+
 export interface IAppState {
 	toast: IToast | null;
+	modal: IModal;
 }
 
 export interface IToast {
 	message: string;
 	type: "success" | "error" | "info";
 	duration?: number;
+}
+
+export interface IModal {
+	isOpen: boolean;
+	title: string | null;
+	type: "editCard" | "createCard" | null;
+	props: ITaskCardProps | null;
 }
